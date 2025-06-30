5dc067149b82b82cdc8830028b20a412a527414b
curreesan <115443503+curreesan@users.noreply.github.com>
2025-06-30 14:46:12 +0530

    Add a feature, implementing getComputerChoice function that returns either stone, paper or scissors

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log("Hellow World")
+
+function getComputerChoice() {
+    let number = Math.floor(Math.random()* 3)
+    console.log(number)
+    if (number === 0) {
+        return "rock"
+    } else if (number === 1) {
+        return "paper"
+    } else return "scissors"
+}
+
+const computerChoice = getComputerChoice()
+console.log(computerChoice)
