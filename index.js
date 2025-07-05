56696f8839367cca0cdbc787ad72739fb2e784c9
curreesan <115443503+curreesan@users.noreply.github.com>
2025-07-06 01:08:25 +0530

    Add final logic for UI game, and CSS

--- index.js
+++ index.js
@@ -3,15 +3,21 @@
 //track scores
 let humanScore = 0;
 let computerScore = 0;
+let gameNumber = 1;
 
 const buttons = document.querySelector(".buttons");
 
-const display = document.querySelector(".display-result")
-const score = document.querySelector(".display-score")
+const gameCount = document.querySelector(".game-count")
+const display = document.querySelector(".display-result");
+const score = document.querySelector(".display-score");
+const winner = document.querySelector(".display-winner");
 
 buttons.addEventListener('click', function(e){
-    playRound(e.target.id);
-})
+    if((humanScore <5 && computerScore <5) && (e.target.id == "rock" || e.target.id == "paper" || e.target.id == "scissors")) {
+            playRound(e.target.id);
+    }else {
+        alert("Click on the actual buttons, idiot")
+    }})
 
 //simulate a random choice(either rock,paper or scissors) from Computer
 function getComputerChoice() {
@@ -24,49 +30,65 @@ function getComputerChoice() {
     } else return "scissors"
 }
 
-//returns final result after 5 games 
-function finalWinnerDeclaration(scoreHuman, scoreComputer) {
-    if (scoreHuman == scoreComputer) return `It's a DRAW as both scores are ${scoreHuman}`; 
-
-    return (scoreHuman > scoreComputer) ? `You WIN as you beat computer ${scoreHuman} times!` : `You LOSE as computer beat you ${scoreComputer} times!`
+//display winner after one of them gets to 5 wins
+function displayWinner() {
+    winner.textContent =  (humanScore > computerScore) ? `You WIN :)` : `You LOSE :(`
 }
 
+//display text result in the UI after each round
 function displayResult(output) {
     display.textContent = output;
 }
 
-function displayScore(scoreOfHuman,scoreOfComputer) {
-    score.textContent = `YOUR SCORE: ${scoreOfHuman} <> COMPUTER SCORE: ${scoreOfComputer}`
+//display score result in the UI after each round
+function displayScore() {
+    score.textContent = `Your Score: ${humanScore} | Computer Score: ${computerScore}`
+}
+
+function displayGameCount() {
+    gameCount.textContent = gameNumber;
+}
+
+function checkGameEnd() {
+    if (humanScore >= 5 || computerScore >= 5) {
+        displayWinner();
+        // resetGame();
+    }
 }
 
 //1 round logic 
 function playRound(humanChoice) {
-        const computerChoice = getComputerChoice();
 
+        const computerChoice = getComputerChoice();
+        
         //neither win conditions
         if (humanChoice == computerChoice) {
             displayResult(`It's a Draw, both picked ${humanChoice}`)
-            displayScore(humanScore, computerScore);
+            displayScore();
         }
-
+        
         //human win conditions 
         if ((humanChoice == "rock" && computerChoice == "scissors") || 
         (humanChoice == "paper" && computerChoice == "rock") ||
         (humanChoice == "scissors" && computerChoice == "paper")) {
             displayResult(`You win! ${humanChoice} beats ${computerChoice}`)
             humanScore++;
-            displayScore(humanScore, computerScore);
+            displayScore();
         }
-
+        
         //human lose conditions 
         if ((humanChoice == "rock" && computerChoice == "paper") || 
         (humanChoice == "paper" && computerChoice == "scissors") ||
         (humanChoice == "scissors" && computerChoice == "rock")) {
             displayResult(`You lose! ${computerChoice} beats ${humanChoice}`)
             computerScore++;
-            displayScore(humanScore, computerScore);
+            displayScore();
         }
-
+        
+        gameNumber++;
+        displayGameCount();
+        checkGameEnd();    
+        
     }
-
+    
 
